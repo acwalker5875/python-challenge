726a31a314e59738a4933a268c144cf337756630
acwalker5875 <137699333+acwalker5875@users.noreply.github.com>
2023-09-30 09:54:55 -0400

    updating main file

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,25 +8,27 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter ='')
 
     print(csvreader)
-#     csv_header = next(csvreader)
-#     print(f"CSV Header: {csv_header}")
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
-#     for row in csvreader:
-#         print(row)
+    for row in csvreader:
+        print(row)
     
 
 #     rows= csvfile.readlines()
 # print(len(rows))
 
 # The total number of months included in the dataset
-# total_months = len(column(0))
+total_months = int(len(column(0)))
 
-# # The net total amount of "Profit/Losses" over the entire period
-# total_profit = sum(range(column(1)))
-# # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+# The net total amount of "Profit/Losses" over the entire period
 
-# avg_profit = total_profit / total_months
+total_profit = sum(int(range(row[1])))
 
-# # The greatest increase in profits (date and amount) over the entire period
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
-# # The greatest decrease in profits (date and amount) over the entire period
+avg_profit = int(total_profit / total_months)
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in profits (date and amount) over the entire period
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,22 @@
+import os
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter ='')
+
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)
+
 # The total number of votes cast
+
+total_votes = int(len(column(0))) 
 
 # A complete list of candidates who received votes
 
